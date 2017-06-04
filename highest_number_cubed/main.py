47da8ac43f87d45df17ffa7f6c8ce674d87b42c6
Kyle <mcdougle.kyle@gmail.com>
2017-06-04 14:35:37 +0000

    Yay! first project!

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -1,5 +1,11 @@
 """This is the entry point of the program."""
-
-
 def highest_number_cubed(limit):
-    pass
+    answer= 0
+    while answer**3 < limit:
+        answer+=1
+    else:
+        return answer - 1
+
+        
+        
+        
